c9d5e831b19e0d0dae3b8fb3d3e6a2db6fd49230
Valentin <latronixo@gmail.com>
2025-04-26 18:25:49 +0300

    Initial Commit

---
+++ 19. Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19. Project 2
+//
+//  Created by Валентин Картошкин on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
